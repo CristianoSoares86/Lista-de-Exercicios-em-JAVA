4f2102e62ff16d51af87d312e7054f27585016e6
CristianoSoares86 <soares.cristiano86@gmail.com>
2021-10-20 21:00:12 -0300

    criando a primeira lista

---
+++ Listas de Exercicios/src/br/com/listasDeExercicios/lista01/Questao01.java
@@ -0,0 +1,15 @@
+package br.com.listasDeExercicios.lista01;
+
+public class Questao01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		System.out.println("OIIII");
+
+	}
+
+}
